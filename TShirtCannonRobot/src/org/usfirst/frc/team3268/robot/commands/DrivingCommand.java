edbd3c98c6f5b7f9b555699a162ba08e6724df8b
Volcore001 <36526721+Volcore001@users.noreply.github.com>
2018-11-15 13:00:23 -0800

    Actual project
    Previous commit was incorrect, will delete soon

--- TShirtCannonRobot/src/org/usfirst/frc/team3268/robot/commands/DrivingCommand.java
+++ TShirtCannonRobot/src/org/usfirst/frc/team3268/robot/commands/DrivingCommand.java
@@ -25,7 +25,7 @@ public class DrivingCommand extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		Robot.driveTrain.tankDrive(OI.stick);
+		Robot.driveTrain.ArcadeDrive(OI.stick.getY(), OI.stick.getX());
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
